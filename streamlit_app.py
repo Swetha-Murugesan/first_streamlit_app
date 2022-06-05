4e610d713a65d30c55464d48bb81b976924f806d
Swetha M <106866352+Swetha-Murugesan@users.noreply.github.com>
2022-06-05 17:14:52 +0530

    Update streamlit_app.py
    header for special menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 streamlit.title('My Parents New Healthy diner')
-streamlit.header('Breakfast menu')
+streamlit.header('🍲Breakfast menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, Spinach & Rocket Smoothy')
-streamlit.text('Hard-Boiled free range egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬kale, Spinach & Rocket Smoothy')
+streamlit.text('🐔Hard-Boiled free range egg')
+streamlit.text('🥑avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
