cee18c3c14ebd09b11341c7184fd803f0922bc7d
Swetha M <106866352+Swetha-Murugesan@users.noreply.github.com>
2022-06-05 17:36:57 +0530

    Update streamlit_app.py
    to show selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,5 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 # streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick Some fruites :",list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
