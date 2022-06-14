e4adbf011874f98a1524fea97d24d92c80727abf
Swetha M <106866352+Swetha-Murugesan@users.noreply.github.com>
2022-06-14 16:29:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title('My Parents New Healthy diner')
@@ -11,7 +15,7 @@ streamlit.text('🥑avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 # streamlit.dataframe(my_fruit_list)
@@ -25,14 +29,14 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice=streamlit.text_input('what fruit would you like to infromation about?','kiwi')
 streamlit.write('The user entered',fruit_choice)
 
-import requests
+#import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice) 
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
